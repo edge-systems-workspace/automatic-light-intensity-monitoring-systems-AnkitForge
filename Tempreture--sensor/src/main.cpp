8533f0692e6277171bf4c609ff632f28ee11faea
Ankit Patel <ankit.patel_cs.aiml24@gla.ac.in>
2026-02-05 10:06:15 +0530

    Tempreture sensor

---
+++ Tempreture--sensor/src/main.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+#include <DHT.h>
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+    Serial.begin(9600);
+    dht.begin();
+    Serial.println("DHT11 initialized");
+}
+
+void loop() {
+    float humidity = dht.readHumidity();
+    float temperature = dht.readTemperature();
+    if (isnan(humidity)||isnan(temperature)) {
+        Serial.println("Failed to read from DHT sensor!");
+        delay(500);
+        return;
+    }
+    Serial.print("Temperature: ");
+    Serial.println(temperature);
+    Serial.println(" C ");
+    Serial.print("Humidity: ");
+    Serial.println(humidity);
+    Serial.println(" %  | ");
+    delay(500);
+
+
+}
